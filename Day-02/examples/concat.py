2709392aacbb92b06da6c4d84e0a30024fdcc309
pkurra75developer <148650563+pkurra75developer@users.noreply.github.com>
2023-11-07 13:30:58 +0000

    new files added

---
+++ Day-02/examples/concat.py
@@ -0,0 +1,5 @@
+s1 = "Hello"
+s2 = "Praveen"
+con = s1 + " " + "Indiana Jones" + " " + "Kurra" + " " + s2
+print(con)
+
---
+++ Day-02/examples/upper_lower.py
@@ -0,0 +1,12 @@
+text = "Quick mad fox jumps over a lazy hen"
+str1 = "THIS IS ALL IN UPPER CASE"
+str2 = "this is all in lower case"
+
+text1 = len(text)
+res1 = str1.lower()
+res2 = str2.upper()
+
+print (text1)
+print (res1)
+print (res2)
+
